deece49d12c90ac0e31d8c36a2b92e25ce637d52
ROIM1998 <zhaobw1998@sina.com>
2022-11-07 15:51:39 -0800

    Seperate tic-tac-toe functions into CLI and logic

--- cli.py
+++ cli.py
@@ -2,16 +2,41 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, move, get_winner, other_player, board_to_str, chr_to_int, check_draw
 
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
+    now = 'O'
     while winner == None:
-        print("TODO: take a turn!")
         # TODO: Show the board to the user.
+        print(board_to_str(board))
         # TODO: Input a move from the player.
+        print("TODO: take a turn!")
+        position = input("Player %s please input the position you want to take, for example, \"a 0\"" % now)
+        row, col = position.split()
+        if row not in ['a', 'b', 'c']:
+            print("Wrong input! The index of rows should be 'a', 'b', or 'c'")
+            continue
+        if col not in ['0', '1', '2']:
+            print("Wrong input! The index of columns should be '0', '1', or '2'")
+            continue
         # TODO: Update the board.
+        success = move(board, now, (chr_to_int[row], int(col)))
         # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+        if success:
+            now = other_player(now)
+        checked = get_winner(board)
+        if checked != 0:
+            winner = checked
+            print(board_to_str(board))
+            print("Player %s has won!!" % checked)
+        if check_draw(board):
+            print(board_to_str(board))
+            print("Draw!")
+            break
+        
+        
+        
+        
--- logic.py
+++ logic.py
@@ -1,22 +1,61 @@
 # This file is where game logic lives. No input
 # or output happens here. The logic in this file
 # should be unit-testable.
+import numpy as np
+
+cond_to_chr = {0: ' ', 1: 'O', 2: 'X'}
+chr_to_int = {'a': 0, 'b': 1, 'c': 2}
+int_to_role = {1: 'O', 2: 'X'}
+role_to_int = {'O': 1, 'X': 2}
 
 
 def make_empty_board():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+    return np.zeros([3,3])
+
+
+def check_winner(board, player):
+    checks = []
+    checks += [all(board[0] == player), all(board[1] == player), all(board[2] == player)]
+    checks += [all(board[:, 0] == player), all(board[:, 1] == player), all(board[:, 2] == player)]
+    checks += [all(board.flatten()[0::4] == player), all(board.flatten()[2:7:2] == player)]
+    return any(checks)
+
+
+def check_draw(board):
+    return all(board.flatten() != 0)
 
 
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    if check_winner(board, 1):
+        return int_to_role[1]
+    elif check_winner(board, 2):
+        return int_to_role[2]
+    else:
+        return 0
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return "O" if player == "X" else "X" 
+
+
+def move(board, player, position):
+    if board[position[0]][position[1]] == 0:
+        board[position[0]][position[1]] = role_to_int[player]
+        return True
+    else:
+        print("The place has already been taken!")
+        return False
+    
+    
+def board_to_str(board):
+    separator = '---------------\n'
+    header = '    0   1   2  \n'
+    row_index='a'
+    row_template = '%s | %s | %s | %s |\n'
+    row_strings = []
+    for i in range(3):
+        row_strings.append(row_template % (chr(ord(row_index)+i), cond_to_chr[board[i][0]], cond_to_chr[board[i][1]], cond_to_chr[board[i][2]]))
+    return header + separator.join(row_strings)
