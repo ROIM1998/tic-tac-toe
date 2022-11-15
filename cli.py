8d14b361d9072dce118c9cba8ccfd1f4ba5808e6
ROIM1998 <zhaobw1998@sina.com>
2022-11-14 17:20:02 -0800

    Using class to encapsulate the game

--- cli.py
+++ cli.py
@@ -2,16 +2,16 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board, move, get_winner, other_player, board_to_str, chr_to_int, check_draw
+from logic import Game, chr_to_int
 
 
 if __name__ == '__main__':
-    board = make_empty_board()
     winner = None
     now = 'O'
+    game = Game()
     while winner == None:
         # TODO: Show the board to the user.
-        print(board_to_str(board))
+        print(game)
         # TODO: Input a move from the player.
         print("TODO: take a turn!")
         position = input("Player %s please input the position you want to take, for example, \"a 0\"" % now)
@@ -23,17 +23,17 @@ if __name__ == '__main__':
             print("Wrong input! The index of columns should be '0', '1', or '2'")
             continue
         # TODO: Update the board.
-        success = move(board, now, (chr_to_int[row], int(col)))
+        success = game.move(now, (chr_to_int[row], int(col)))
         # TODO: Update who's turn it is.
         if success:
-            now = other_player(now)
-        checked = get_winner(board)
+            now = game.other_player(now)
+        checked = game.get_winner()
         if checked != 0:
             winner = checked
-            print(board_to_str(board))
+            print(game)
             print("Player %s has won!!" % checked)
-        if check_draw(board):
-            print(board_to_str(board))
+        if game.check_draw():
+            print(game)
             print("Draw!")
             break
         
--- logic.py
+++ logic.py
@@ -6,56 +6,71 @@ import numpy as np
 cond_to_chr = {0: ' ', 1: 'O', 2: 'X'}
 chr_to_int = {'a': 0, 'b': 1, 'c': 2}
 int_to_role = {1: 'O', 2: 'X'}
-role_to_int = {'O': 1, 'X': 2}
-
+role_to_int = {'O': 1, 'X': 2, None: 0}
 
 def make_empty_board():
     return np.zeros([3,3])
 
 
-def check_winner(board, player):
-    checks = []
-    checks += [all(board[0] == player), all(board[1] == player), all(board[2] == player)]
-    checks += [all(board[:, 0] == player), all(board[:, 1] == player), all(board[:, 2] == player)]
-    checks += [all(board.flatten()[0::4] == player), all(board.flatten()[2:7:2] == player)]
-    return any(checks)
+class Game:
+    def __init__(self):
+        self.board = make_empty_board()
 
 
-def check_draw(board):
-    return all(board.flatten() != 0)
+    def check_winner(self, player):
+        checks = []
+        checks += [all(self.board[0] == player), all(self.board[1] == player), all(self.board[2] == player)]
+        checks += [all(self.board[:, 0] == player), all(self.board[:, 1] == player), all(self.board[:, 2] == player)]
+        checks += [all(self.board.flatten()[0::4] == player), all(self.board.flatten()[2:7:2] == player)]
+        return any(checks)
 
 
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    if check_winner(board, 1):
-        return int_to_role[1]
-    elif check_winner(board, 2):
-        return int_to_role[2]
-    else:
-        return 0
+    def check_draw(self):
+        return all(self.board.flatten() != 0)
 
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O" if player == "X" else "X" 
+    def get_winner(self):
+        """Determines the winner of the given board.
+        Returns 'X', 'O', or None."""
+        if isinstance(self.board, list):
+            self.board = np.array([[role_to_int[v] for v in l] for l in self.board])
+        if self.check_winner(1):
+            return int_to_role[1]
+        elif self.check_winner(2):
+            return int_to_role[2]
+        else:
+            return 0
 
 
-def move(board, player, position):
-    if board[position[0]][position[1]] == 0:
-        board[position[0]][position[1]] = role_to_int[player]
-        return True
-    else:
-        print("The place has already been taken!")
-        return False
-    
-    
-def board_to_str(board):
-    separator = '---------------\n'
-    header = '    0   1   2  \n'
-    row_index='a'
-    row_template = '%s | %s | %s | %s |\n'
-    row_strings = []
-    for i in range(3):
-        row_strings.append(row_template % (chr(ord(row_index)+i), cond_to_chr[board[i][0]], cond_to_chr[board[i][1]], cond_to_chr[board[i][2]]))
-    return header + separator.join(row_strings)
+    def other_player(self, player):
+        """Given the character for a player, returns the other player."""
+        return "O" if player == "X" else "X" 
+
+
+    def move(self, player, position):
+        if self.board[position[0]][position[1]] == 0:
+            self.board[position[0]][position[1]] = role_to_int[player]
+            return True
+        else:
+            print("The place has already been taken!")
+            return False
+        
+        
+    def __repr__(self):
+        separator = '---------------\n'
+        header = '    0   1   2  \n'
+        row_index='a'
+        row_template = '%s | %s | %s | %s |\n'
+        row_strings = []
+        for i in range(3):
+            row_strings.append(row_template % (chr(ord(row_index)+i), cond_to_chr[self.board[i][0]], cond_to_chr[self.board[i][1]], cond_to_chr[self.board[i][2]]))
+        return header + separator.join(row_strings)
+
+
+class Bot:
+    def __init__(self, mode):
+        self.mode = mode
+        
+    def get_move(self, board):
+        indices = np.where(board == 0)
+        available = [(i, j) for i, j in zip(indices[0], indices[1])]
