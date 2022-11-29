a77f97cb6ec309ea92beaed2aa83e7b0b1dc9587
ROIM1998 <zhaobw1998@sina.com>
2022-11-29 12:28:04 -0800

    Add argparse in the main files

--- cli.py
+++ cli.py
@@ -3,21 +3,36 @@
 # For core game logic, see logic.py.
 
 from logic import Game
+import argparse
+
+parser = argparse.ArgumentParser(description='Play Tic-Tac-Toe.')
+parser.add_argument('--player1', default='none', choices=['human', 'random', 'minimax'])
+parser.add_argument('--player2', default='none', choices=['human', 'random', 'minimax'])
+args = parser.parse_args()
+playertype2abbreviation = {
+    'human': 'p',
+    'random': 'r',
+    'minimax': 'e',
+}
 
 
 if __name__ == '__main__':
-    game_mode = None
-    while game_mode not in ['pvp', 'pve', 'eve', 'evr']:
-        game_mode = input('Choose game mode (pvp, pve, eve, or evr): ')
-        if game_mode not in ['pvp', 'pve', 'eve', 'evr']:
-            print('Invalid game mode. Please try again.')
-    if game_mode == 'pvp' or game_mode == 'eve' or game_mode == 'evr':
-        start_first = True
+    if args.player1 == 'none' or args.player2 == 'none':
+        game_mode = None
+        while game_mode not in ['pvp', 'pve', 'eve', 'evr']:
+            game_mode = input('Choose game mode (pvp, pve, eve, or evr): ')
+            if game_mode not in ['pvp', 'pve', 'eve', 'evr']:
+                print('Invalid game mode. Please try again.')
+        if game_mode == 'pvp' or game_mode == 'eve' or game_mode == 'evr':
+            start_first = True
+        else:
+            start_command = input('Do you want to start first? (y/n): ')
+            while start_command not in ['y', 'n']:
+                start_command = input('Invalid input. Please input "y" or "n": ')
+            start_first = start_command == 'y'
     else:
-        start_command = input('Do you want to start first? (y/n): ')
-        while start_command not in ['y', 'n']:
-            start_command = input('Invalid input. Please input "y" or "n": ')
-        start_first = start_command == 'y'
+        game_mode = playertype2abbreviation[args.player1] + 'v' + playertype2abbreviation[args.player2]
+        start_first = True
     game = Game(game_mode=game_mode, start_first=start_first)
     game.start()
         
