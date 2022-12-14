d3d370b197c8746ff3fde5f9ef3144549288429e
ROIM1998 <zhaobw1998@sina.com>
2022-12-14 11:34:28 -0800

    Using in-python command to start app

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/tic-tac-toe")
 def hello_world():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
